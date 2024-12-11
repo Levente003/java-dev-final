b3b479ae404113bfaba35d90b6763ecce559dd75
Szénási Ervin Levente <szenasiocsi@gmail.com>
2024-12-11 21:22:37 +0100

    fixed screening

--- ticket-service/src/main/java/com/epam/training/ticketservice/ui/commands/ScreeningCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/ui/commands/ScreeningCommand.java
@@ -1,5 +1,6 @@
 package com.epam.training.ticketservice.ui.commands;
 
+import com.epam.training.ticketservice.core.movie.MovieService;
 import com.epam.training.ticketservice.core.screening.ScreeningService;
 import com.epam.training.ticketservice.core.screening.model.ScreeningDto;
 import lombok.AllArgsConstructor;
@@ -9,17 +10,33 @@ import org.springframework.shell.standard.ShellMethod;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 
 @ShellComponent
 @AllArgsConstructor
 public class ScreeningCommand {
     private final ScreeningService screeningService;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private final MovieService movieService;
 
     @ShellMethod(value = "Create a new screening", key = "create screening")
     public String createScreening(String movieTitle, String roomName, String startTime) {
         LocalDateTime startDateTime = LocalDateTime.parse(startTime, formatter);
-
+        boolean badTime = screeningService.listScreenings().stream().anyMatch(screeningDto -> screeningDto.startTime().isBefore(startDateTime.plusMinutes(movieService.listMovies()
+                                                                                                                                                                                  .stream()
+                                                                                                                                                                                  .filter(movieDto -> Objects.equals(movieDto.title(), movieTitle)).findFirst().get().length()))
+                                                                                && screeningDto.startTime().plusMinutes(screeningDto.movie().getLength()).isAfter(startDateTime)
+                                                                                && screeningDto.room().getName().equals(roomName));
+        boolean breakTime = screeningService.listScreenings().stream().anyMatch(screeningDto -> screeningDto.startTime().isBefore(startDateTime.plusMinutes(movieService.listMovies()
+                .stream().filter(movieDto -> Objects.equals(movieDto.title(), movieTitle)).findFirst().get().length()+10))
+                && screeningDto.startTime().plusMinutes(screeningDto.movie().getLength()+10).isAfter(startDateTime)
+                && screeningDto.room().getName().equals(roomName));
+        if (badTime) {
+            return "There is an overlapping screening";
+        }
+        if (breakTime) {
+            return "This would start in the break period after another screening in this room";
+        }
         screeningService.createScreening(movieTitle, roomName, startDateTime);
         return "Screening created";
     }
